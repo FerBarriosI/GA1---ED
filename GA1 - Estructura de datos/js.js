2007a49aa949650d9ab0962a2ac5db6d94e90741
FerBarriosI <157501838+FerBarriosI@users.noreply.github.com>
2025-03-14 22:46:26 -0600

    Add files via upload

---
+++ GA1 - Estructura de datos/js.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById("preloader").style.display = "none";
+        document.getElementById("contenido").style.display = "block";
+    }, 3000);
+};
